742310bffb285ab9a0033f0535410b93f29d70fb
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-02-07 12:14:57 -0800

    Initial commit - beginning lab 4A

---
+++ 210-lab-4A.cpp
@@ -0,0 +1,8 @@
+//*****************************************************************************
+// COMSC-210 | Lab 4A | Gabriel Marquez
+// Description: this program create and store a vector of structs.
+//*****************************************************************************
+
+#include <iostream>
+#include <vector>
+using namespace std;
