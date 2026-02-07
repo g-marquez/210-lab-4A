76eb896a338a1d0fbdc85048d62088bf2ed17eb1
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-02-07 12:28:54 -0800

    Milestone 1: example color struct created and output to the console

--- 210-lab-4A.cpp
+++ 210-lab-4A.cpp
@@ -6,3 +6,28 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+struct Color
+{
+    int red_val;
+    int blue_val;
+    int green_val;
+};
+
+int main()
+{
+    //declare and initialize example Color object with values
+    Color example;
+    example.red_val = 12;
+    example.blue_val = 23;
+    example.green_val = 83;
+
+    //output example color struct
+    cout << "Example color struct:" << endl;
+    cout << "---------------------" << endl;
+    cout << "Red Value: " << example.red_val << endl;
+    cout << "Blue Value: " << example.blue_val << endl;
+    cout << "Green Value: " << example.green_val << endl;
+    
+    return 0;
+}
