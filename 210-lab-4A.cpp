7650ade0b707fe401ee143a426d3adf9b53c2a40
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-02-07 13:28:51 -0800

    update: RNG value does not change, trying to fix

--- 210-lab-4A.cpp
+++ 210-lab-4A.cpp
@@ -44,20 +44,35 @@ int main()
              << colorVec[i].green_val << endl;
     }
 
+    //clearing colorVec for next Milestone
+    colorVec.clear();
+
     //RNG between 25 and 50 (following online example, pending any response on
     // Module 2 discussion board)
     random_device rd; //seed source for random number engine using <random>
     mt19937 gen(rd()); //mersenne_twister_engine seeded with rd()
     uniform_int_distribution<> distrib(25, 50);
     int n = distrib(gen); //a random number n between 25 and 50
-    uniform_int_distribution<> rand_int();
+    uniform_int_distribution<> rand_int(1, 1000); //arbitrary range selected
     int random_color_val = rand_int(gen); //random number for filling color member values
 
     //push_back n colors into colorVec
     for (int i = 0; i < n; ++i)
     {
         Color temp;
-
+        temp.red_val = random_color_val;
+        temp.blue_val = random_color_val;
+        temp.green_val = random_color_val;
+        colorVec.push_back(temp);
     }
+
+    //output colorVec with n elements
+    for ( int i = 0; i < colorVec.size(); ++i)
+    {
+        cout << "Color #" << i+1 << endl;
+        cout << colorVec[i].red_val << " " << colorVec[i].blue_val << " "
+             << colorVec[i].green_val << endl << endl;
+    }
+
     return 0;
 }
